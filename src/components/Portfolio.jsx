c3a44e832a986845ab8de749ab0559947109157b
Pawel <webloopbox@gmail.com>
2023-07-24 18:20:33 +0200

    update portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,35 +3,46 @@ import UnderLine from "./UnderLineEffect";
 import Fade from "react-reveal/Fade";
 
 class Portfolio extends React.Component {
-
   componentDidMount() {
     const portfolio_items = document.getElementsByClassName("portfolio-item");
 
-    if(portfolio_items.length % 2 == 1) {
-      portfolio_items[portfolio_items.length-1].style.margin = "50px auto 0 auto"
+    if (portfolio_items.length % 2 == 1) {
+      portfolio_items[portfolio_items.length - 1].style.margin =
+        "50px auto 0 auto";
     }
-
   }
 
   render() {
     const details = [
       {
-        name: "serwis komputerowy",
-        src: "./assets/strona1.jpg",
-        tech: "HTML / CSS / JS / WORDPRESS",
+        name: "Landing Page",
+        src: "./assets/portfolio-website1-thumbnail.jpg",
+        tech: "HTML / CSS / JS / WORDPRESS (Custom theme)",
         link: "https://strona1.pawelmarek.com",
       },
       {
-        name: "CREATIX",
-        src: "./assets/strona2.jpg",
-        tech: "HTML / CSS / JS / WORDPRESS",
+        name: "Landing Page",
+        src: "./assets/portfolio-website2-thumbnail.jpg",
+        tech: "HTML / CSS / JS / WORDPRESS (Custom theme)",
         link: "https://strona2.pawelmarek.com",
       },
       {
-        name: "CREATIX",
-        src: "./assets/weatherapp.jpg",
+        name: "Weather App",
+        src: "./assets/weather-app-thumbnail.png",
         tech: "REACT.JS",
         link: "https://weather.pawelmarek.com",
+      },
+      {
+        name: "Netflix Clone",
+        src: "./assets/netflix-clone-thumbnail.png",
+        tech: "REACT.JS",
+        link: "https://github.com/webloopbox/netflix-clone",
+      },
+      {
+        name: "Route Mapping App",
+        src: "./assets/maps-app-thumbnail.png",
+        tech: "REACT.JS",
+        link: "https://magnificent-trifle-4db8fa.netlify.app",
       },
     ];
 
@@ -58,6 +69,8 @@ class Portfolio extends React.Component {
                     <img src={item.src} alt="portfolio-thumbnail" />
                     <div className="thumbnail-overlay">
                       <div className="overlay-content">
+                        {item.name}
+                        <br />
                         {item.tech}
                         <button className="check">Sprawdź</button>
                       </div>
@@ -66,6 +79,15 @@ class Portfolio extends React.Component {
                 </div>
               </Fade>
             ))}
+            <Fade up delay={300 * details.length}>
+              <a
+                target="_blank"
+                href="https://github.com/webloopbox/WeatherApp"
+                className="portfolio-item portfolio-item-last"
+              >
+                Check for more on my github
+              </a>
+            </Fade>
           </div>
         </div>
         <img id="gray-wave" src="assets/gray_wave.svg" />
